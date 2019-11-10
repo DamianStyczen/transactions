6205836a17a6e51c205d834e0af718fd04815043
Damian Styczeń <styczen.damian@gmail.com>
2019-11-10 15:08:12 +0100

    Delete css file

--- src/App.style.js
+++ src/App.style.js
@@ -4,6 +4,15 @@ export const GlobalStylesProvider = createGlobalStyle`
 * {
     box-sizing: border-box;
 };
+
+body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
 `;
 export const Wrapper = styled.main`
     padding: 20px;
